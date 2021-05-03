dfbaa070f17f7f3fc6703aaa6202367e46b509ef
Samin1615088 <samin.shafkat@gmail.com>
2021-05-03 12:52:04 +0600

    project filter functionality added

--- js/main.js
+++ js/main.js
@@ -22,3 +22,41 @@
         }
     })
 })();
+
+
+/* -------------------------------- portfolio filter and popup menu --------------------------------*/
+(() => {
+
+    const filterContainer = document.querySelector(".portfolio-filter");
+    const filterItemsContainer = document.querySelector(".portfolio-items");
+    const portfolioItems = document.querySelectorAll(".portfolio-item");
+    const popup = document.querySelector(".portfolio-popup");
+    const prevBtn = popup.querySelector(".pp-prev");
+    const nextBtn = popup.querySelector(".pp-next");
+    const closeBtn = popup.querySelector(".pp-close");
+    const projectDetailsContainer = popup.querySelector(".pp-details");
+    const projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+    let itemIndex, slideIndex, screenShots;
+
+    /* filter portfolio items */
+    filterContainer.addEventListener("click", (event) => {
+        if (event.target.classList.contains("filter-item") && !event.target.classList.contains(".active")) {
+            //deactivate existing active 'filter item'
+            filterContainer.querySelector(".active").classList.remove("active", "outer-shadow");
+            // activate new 'filter item'
+            event.target.classList.add("active", "outer-shadow");
+            const target = event.target.getAttribute("data-target");
+            portfolioItems.forEach(item => {
+                if (target === item.getAttribute("data-category") || target === "all") {
+                    item.classList.remove("hide");
+                    item.classList.add("show");
+                    console.log(item)
+                } else {
+                    item.classList.remove("show");
+                    item.classList.add("hide");
+                }
+            })
+        }
+    })
+
+})();
